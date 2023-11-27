0e02a495a65d46eb76045552244595a675ce720b
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2023-11-28 00:55:08 +0300

    Make form handling with default NodeJS packages

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const express = require("express")
-const app = express()
+const http = require("http")
+const fs = require("fs")
 
-let message = "Hello world"
-app.get("/", function (request, response) {
-    console.log(message)
-    response.end(message)
-})
+http.createServer(async (request, response) => {
+    if (request.url === "/user") {
+        let body = ""
+        for await (const chunk of request) {
+            body += chunk
+        }
+        
+        let userName
+        let userAge
+        const fields = body.split("&")
+        for (const field of fields){
+            const params = field.split("=")
+            if (params[0] === "username") userName = params[1]
+            if (params[0] === "userage") userAge = params[1]
+        }
 
-app.listen(3000, "localhost")
+        console.log(`User name: ${userName}\nUser age: ${userAge}`)
+        response.end(`Your name: ${userName}\nYour age: ${userAge}`)
+    } 
+    else {
+        fs.readFile("index.html", (error, data) => response.end(data))
+    }
+}).listen(3000)
